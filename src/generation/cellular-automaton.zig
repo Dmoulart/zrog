78011b057d3c6f5aa15fca5d7673b8a1d6d88bcd
Dorian <dorian@resalocal.com>
2023-06-11 15:54:28 +0200

    automaton

--- src/generation/cellular-automaton.zig
+++ src/generation/cellular-automaton.zig
@@ -1,66 +1,105 @@
 const std = @import("std");
 
-pub const Cells = enum(u8) {
-    dead = 0,
-    alive = 1,
-};
-
 pub const CellularAutomaton = struct {
     const Self = @This();
 
-    cells: []Cells = undefined,
+    allocator: std.mem.Allocator,
+
+    cells: []Cells,
+    next_cells: []Cells,
 
     height: usize,
     width: usize,
 
-    pub fn init(width: usize, height: usize) Self {
-        var cells: []Cells = undefined;
+    pub const Cells = enum(u8) {
+        dead = 0,
+        alive = 1,
+    };
+
+    pub fn init(allocator: std.mem.Allocator, width: usize, height: usize) !Self {
+        var cells = try allocator.alloc(Cells, width * height);
+        var next_cells = try allocator.alloc(Cells, width * height);
+
+        var automaton = Self{
+            .allocator = allocator,
+            .cells = cells,
+            .next_cells = next_cells,
+            .width = width,
+            .height = height,
+        };
 
         var y: usize = 0;
 
         while (y < height) : (y += 1) {
             var x: usize = 0;
 
-            var row: []Cells = undefined;
-
-            cells[y] = row;
-
             while (x < width) : (x += 1) {
-                cells[y][x] = Cells.Dead;
+                automaton.set(x, y, .dead);
             }
         }
 
-        return Self{
-            .cells = cells,
-            .width = width,
-            .height = height,
-        };
+        return automaton;
     }
 
-    pub fn populate(self: *Self, x: usize, y: usize) void {
-        self.cells[x][y] = Cells.Alive;
+    pub fn set(self: *Self, x: usize, y: usize, state: Cells) void {
+        self.cells[y * self.width + x] = state;
     }
 
-    pub fn update(self: *Self) void {
+    pub fn get(self: *Self, x: usize, y: usize) *Cells {
+        return &self.cells[y * self.width + x];
+    }
+
+    pub fn map(self: *Self, function: *const fn (x: usize, y: usize, state: *Cells) Cells) void {
+        var y: usize = 0;
+        while (y < self.height - 1) : (y += 1) {
+            var x: usize = 0;
+
+            while (x < self.width - 1) : (x += 1) {
+                var cell = self.get(x, y);
+                cell.* = function(x, y, self.get(x, y));
+            }
+        }
+    }
+
+    pub fn each(self: *Self, param: anytype, function: *const fn (param: anytype, x: usize, y: usize, state: *Cells) void) void {
+        var y: usize = 0;
+        while (y < self.height - 1) : (y += 1) {
+            var x: usize = 0;
+
+            while (x < self.width - 1) : (x += 1) {
+                function(param, x, y, self.get(x, y));
+            }
+        }
+    }
+
+    pub fn update(self: *Self, n: u32) void {
+        var i: u32 = 0;
+        while (i < n) : (i += 1) {
+            self.step();
+        }
+    }
+
+    pub fn step(self: *Self) void {
         var y: usize = 1;
+
         var neighbors: [8]Cells = undefined;
 
-        var next_cells: []Cells = undefined;
+        var next_cells = self.allocator.alloc(Cells, self.width * self.height) catch unreachable;
 
         while (y < self.height - 1) : (y += 1) {
             var x: usize = 1;
 
             while (x < self.width - 1) : (x += 1) {
-                const left = self.cells[y][x - 1];
-                const right = self.cells[y][x + 1];
+                const left = self.get(x - 1, y).*;
+                const right = self.get(x + 1, y).*;
 
-                const top = self.cells[y - 1][x];
-                const top_left = self.cells[y - 1][x - 1];
-                const top_right = self.cells[y - 1][x + 1];
+                const top = self.get(x, y - 1).*;
+                const top_left = self.get(x - 1, y - 1).*;
+                const top_right = self.get(x + 1, y - 1).*;
 
-                const bottom = self.cells[y + 1][x];
-                const bottom_left = self.cells[y + 1][x - 1];
-                const bottom_right = self.cells[y + 1][x + 1];
+                const bottom = self.get(x, y + 1).*;
+                const bottom_left = self.get(x - 1, y + 1).*;
+                const bottom_right = self.get(x + 1, y + 1).*;
 
                 neighbors = [_]Cells{
                     left,
@@ -76,22 +115,24 @@ pub const CellularAutomaton = struct {
                 var alive_neighbors: u8 = 0;
 
                 for (neighbors) |neighbor| {
-                    alive_neighbors = if (neighbor == .Alive) alive_neighbors + 1 else alive_neighbors;
+                    alive_neighbors = if (neighbor == .alive) alive_neighbors + 1 else alive_neighbors;
                 }
 
-                if (self.cells[y][x] == .Alive) {
+                var current_cell = self.get(x, y).*;
+
+                if (current_cell == .alive) {
                     // If the cell is alive, then it stays alive if it has either 2 or 3 live neighbors.
                     if (alive_neighbors < 2 or alive_neighbors > 3) {
-                        next_cells[y][x] = .Dead;
+                        self.set(x, y, .dead);
                     } else {
-                        next_cells[y][x] = .Alive;
+                        self.set(x, y, .alive);
                     }
                 } else {
                     // If the cell is dead, then it springs to life only in the case that it has 3 live neighbors.
                     if (alive_neighbors == 3) {
-                        next_cells[y][x] = Cells.Alive;
+                        self.set(x, y, .dead);
                     } else {
-                        next_cells[y][x] = Cells.Dead;
+                        self.set(x, y, .alive);
                     }
                 }
             }
--- src/map/forest.zig
+++ src/map/forest.zig
@@ -1,14 +1,21 @@
 const std = @import("std");
 const rl = @import("raylib");
+
 const Zecs = @import("zecs");
 const Ecs = @import("../context.zig").Ecs;
+
 const BoundingBox = @import("../math/bounding-box.zig").BoundingBox;
+const CellularAutomaton = @import("../generation/cellular-automaton.zig").CellularAutomaton;
+
+const RndGen = std.rand.DefaultPrng;
 
 pub fn generate(world: *Ecs, offset_x: i32, offset_y: i32, width: u32, height: u32) void {
     var y: i32 = 0;
     var x: i32 = 0;
 
     createTerrain(world, offset_x + x, offset_y + y, width, height);
+
+    createTrees(world, offset_x + x, offset_y + y, width, height);
 }
 
 pub fn createTerrain(world: *Ecs, x: i32, y: i32, width: u32, height: u32) void {
@@ -45,6 +52,63 @@ pub fn getTerrainBoundingBox(world: *Ecs, terrain: Zecs.Entity) BoundingBox {
         .width = width.*,
         .height = height.*,
     };
+}
+
+pub fn createTrees(world: *Ecs, x: i32, y: i32, width: u32, height: u32) void {
+    _ = y;
+    _ = x;
+    var automaton = CellularAutomaton.init(
+        world.allocator,
+        @intCast(usize, width),
+        @intCast(usize, height),
+    ) catch unreachable;
+
+    automaton.map(populateCellularAutomaton);
+
+    automaton.update(0);
+
+    // automaton.each(world, mapLivingCellToTree);
+
+    var col: usize = 0;
+    while (col < automaton.height - 1) : (col += 1) {
+        var row: usize = 0;
+
+        while (row < automaton.width - 1) : (row += 1) {
+            mapLivingCellToTree(world, row, col, automaton.get(row, col));
+        }
+    }
+}
+
+pub fn populateCellularAutomaton(x: usize, y: usize, state: *CellularAutomaton.Cells) CellularAutomaton.Cells {
+    _ = x;
+    _ = state;
+    _ = y;
+    var rnd = RndGen.init(0);
+    var alive = rnd.random().boolean();
+    return if (alive) .alive else .dead;
+}
+
+pub fn mapLivingCellToTree(world: *Ecs, x: usize, y: usize, state: *CellularAutomaton.Cells) void {
+    if (state.* == .dead) return;
+
+    createTree(world, @intCast(i32, x), @intCast(i32, y));
+}
+
+pub fn createTree(world: *Ecs, x: i32, y: i32) void {
+    var grass = world.createEmpty();
+
+    world.attach(grass, .Transform);
+    world.attach(grass, .Sprite);
+
+    world.write(grass, .Sprite, .{
+        .char = "O",
+        .color = rl.YELLOW,
+    });
+    world.write(grass, .Transform, .{
+        .x = x,
+        .y = y,
+        .z = 0,
+    });
 }
 
 pub fn createGrass(world: *Ecs, x: i32, y: i32) void {
